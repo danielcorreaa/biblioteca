2b69415de888801cd4f17585b86e57382b13d2e6
danielcorrea <danielcorrea@nbbv023701.BRQ.COM>
2016-09-28 11:21:09 -0300

    nova classe para criar login

---
+++ projprime/src/com/projprime/login/Usuario.java
@@ -0,0 +1,37 @@
+package com.projprime.login;
+
+public class Usuario {
+	
+	private String nome;
+	private String senha;
+	private String login;
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	
+	
+	
+
+}
