9e8d3057b9bbd0bf0e70d094867f6e4267296475
danielcorreaa <daniel.cor@outlook.com>
2017-05-20 19:53:46 -0300

    foi adicionado um template em jsp e css

--- biblioteca/src/main/java/org/biblioteca/conf/AppWebConfiguration.java
+++ biblioteca/src/main/java/org/biblioteca/conf/AppWebConfiguration.java
@@ -4,12 +4,14 @@ import org.biblioteca.controller.HomeController;
 import org.biblioteca.dao.AutorDAO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class, AutorDAO.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver(){
@@ -18,5 +20,9 @@ public class AppWebConfiguration {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
--- biblioteca/src/main/webapp/WEB-INF/views/home.jsp
+++ biblioteca/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,18 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Home</title>
-</head>
-<body>
-	<h1>Biblioteca</h1>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="tags" %>
+
+<tags:pageTemplate titulo="Home">
+
 	
-	<ul>
-		<li><a href="autores/form">Autor</a></li>
-		<li><a href="categorias/form">Categoria</a></li>
-		<li><a href="editoras/form">Editora</a></li>
-	</ul>
-</body>
-</html>
+
+</tags:pageTemplate>
